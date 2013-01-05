c53ad610cd0d10991c7f812802a5f04b0f4de23b
Kevin Zweerink <kevin@kevinzweerink.com>
2013-01-05 14:04:47 -0500

    Testing typekit and linking to css files

--- index.php
+++ index.php
@@ -7,9 +7,15 @@
     <script type="text/javascript" src="//use.typekit.net/syj2rgx.js"></script>
     <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
     
+    <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
+
+	<h1>Testing typekit</h1>
+	<p>Testing typekit</p>
+	
+	<?php echo "<p>Testing PHP</p>" ?>
 
 </body>
 </html>
