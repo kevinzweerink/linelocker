92b3c46f0c40828d777a8c12751fdfdabaa080fa
Brannon Dorsey <brannon@brannondorsey.com>
2013-01-05 14:47:15 -0600

    coded index.html

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <title>Linelocker</title>
     
+    <?php include("lib/includes/analytics.inc.php") ?>
     <script type="text/javascript" src="//use.typekit.net/syj2rgx.js"></script>
     <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
     
@@ -11,11 +12,24 @@
 </head>
 
 <body>
-
-	<h1>Testing typekit</h1>
-	<p>Testing typekit</p>
-	
-	<?php echo "<p>Testing PHP</p>" ?>
-
+    <div class="wrapper">
+        <div class="login_container">
+            <img class="login_logo" src="assets/login_logo.png">   
+            <h1>Linelocker</h1>
+            <form>
+                <input type="text" name="username" placeholder="Name">
+                <input type="password" name="password" placeholder="Password">
+                <input type="submit" name="submit" value="Sign In">
+            </form>
+        </div> <!-- login_container-->
+        
+        <div class="info_container">
+            <p>Connect with other slackliners, find spots, share equipment.</p>
+            <a href="tour.php">Take A Tour</a>
+            <a href="register.php">Register Now</a>
+        </div> <!-- info_container -->
+      
+            
+    </div> <!-- wrapper -->
 </body>
 </html>
