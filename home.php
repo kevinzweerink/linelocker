6355f0c5a9639239d804b329d4ec46a56e739db7
Brannon Dorsey <brannon@brannondorsey.com>
2013-01-07 18:39:55 -0600

    Added home.php

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Linelocker</title>
+    
+    <?php include("lib/includes/analytics.inc.php") ?>
+    <script type="text/javascript" src="//use.typekit.net/syj2rgx.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="wrapper">
+        <img src="assets/home_logo.png">
+	    <h1 class="header">linelocker</h1>
+    </div> <!-- wrapper -->
+</body>
+</html>
