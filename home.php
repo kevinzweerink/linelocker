e1f032b2cb26e64039c975ff5242a27e3c1cbe52
Brannon Dorsey <brannon@brannondorsey.com>
2013-01-11 11:48:55 -0600

    preliminary coding and styling of home.php
    Having issues with the CSS of the home.php's header. Header does not
    align properly as .header_buttons falls below the alignment of the
    other header items. .line divs are repeated to properly style for their
    inclusion in .line_container however soon this process will be taken
    over with php.

--- home.php
+++ home.php
@@ -14,16 +14,123 @@
 <body>
     <div class="wrapper">
         <header>
-	    <img src="assets/home_logo.png">
-	    <h1>linelocker</h1>
+	    <a href="home.php" class="homepage_link">
+		<img src="assets/login_logo.png" class="header_logo" width="60em">
+		<h1>linelocker</h1> 
+	    </a>
 	    <form>
 		<input type="text" name="search" placeholder="Search">
-		<a href="browse_by.php">Browse By</a>
-		<a href="new_line.php">Make Line</a>
-		<a href=""
-		
-	    </form>	
+	    </form>
+	    <div class="header_buttons">
+		<a href="browse_by.php" class="browse">Browse By</a>
+		<a href="new_line.php">+ Line</a>
+		<?php //logged in user name
+		    
+		    $logged_in_username='brannondorsey'; //$_GET('logged_in_username');
+		    $user_first = "Brannon"; //SHOW first_name FROM user WHERE username = $logged_in_username;
+		    $user_last = "Dorsey"; //SHOW last_name FROM user WHERE username = $logged_in_username;
+		    $logged_in_name = ucfirst($user_first).' '.ucfirst($user_last);
+		    
+		    //<a> string
+		    echo "<a href='user.php?logged_in_username=$logged_in_username'
+		    class='user_name' style='color: #303030; border: none; padding-left: 1em;'
+		    >$logged_in_name</a>";
+		?>
+		<!-- <img src="assets/carabiner.png" class="carabiner"> -->
+	    </div> <!-- header_buttons -->
 	</header>
+	
+	<div class="line_container">
+	    
+	    <div class="line">
+		<div class="line_header">
+		    <ul>
+			<li>date: 1/3/13</li>
+			<li>time: 11am</li>
+			<li>place: byrd Park</li>
+		    </ul>
+		</div> <!-- line_header -->
+		<p>This is the line description.</p>
+	    </div> <!-- line -->
+	    
+	    <div class="line">
+		<div class="line_header">
+		    <ul>
+			<li>date: 1/3/13</li>
+			<li>time: 11am</li>
+			<li>place: byrd Park</li>
+		    </ul>
+		</div> <!-- line_header -->
+		<p>This is the line description.</p>
+	    </div> <!-- line -->
+	    
+	    <div class="line">
+		<div class="line_header">
+		    <ul>
+			<li>date: 1/3/13</li>
+			<li>time: 11am</li>
+			<li>place: byrd Park</li>
+		    </ul>
+		</div> <!-- line_header -->
+		<p>This is the line description.</p>
+	    </div> <!-- line -->
+	    
+	    <div class="line">
+		<div class="line_header">
+		    <ul>
+			<li>date: 1/3/13</li>
+			<li>time: 11am</li>
+			<li>place: byrd Park</li>
+		    </ul>
+		</div> <!-- line_header -->
+		<p>This is the line description.</p>
+	    </div> <!-- line -->
+	    
+	    <div class="line">
+		<div class="line_header">
+		    <ul>
+			<li>date: 1/3/13</li>
+			<li>time: 11am</li>
+			<li>place: byrd Park</li>
+		    </ul>
+		</div> <!-- line_header -->
+		<p>This is the line description.</p>
+	    </div> <!-- line -->
+	    <div class="line">
+		<div class="line_header">
+		    <ul>
+			<li>date: 1/3/13</li>
+			<li>time: 11am</li>
+			<li>place: byrd Park</li>
+		    </ul>
+		</div> <!-- line_header -->
+		<p>This is the line description.</p>
+	    </div> <!-- line -->
+	    
+	    <div class="line">
+		<div class="line_header">
+		    <ul>
+			<li>date: 1/3/13</li>
+			<li>time: 11am</li>
+			<li>place: byrd Park</li>
+		    </ul>
+		</div> <!-- line_header -->
+		<p>This is the line description.</p>
+	    </div> <!-- line -->
+	    
+	    <div class="line">
+		<div class="line_header">
+		    <ul>
+			<li>date: 1/3/13</li>
+			<li>time: 11am</li>
+			<li>place: byrd Park</li>
+		    </ul>
+		</div> <!-- line_header -->
+		<p>This is the line description.</p>
+	    </div> <!-- line -->
+	    
+	    
+	</div> <!-- line_container -->
     </div> <!-- wrapper -->
 </body>
 </html>
