d97c3800f37be52bd0ad1fd3175462edaacfd685
Kevin Zweerink <kevin@kevinzweerink.com>
2013-01-11 13:30:17 -0500

    Fixed CSS for chrome at least

--- home.php
+++ home.php
@@ -14,30 +14,34 @@
 <body>
     <div class="wrapper">
         <header>
-	    <a href="home.php" class="homepage_link">
-		<img src="assets/login_logo.png" class="header_logo" width="60em">
-		<h1>linelocker</h1> 
-	    </a>
-	    <form>
-		<input type="text" name="search" placeholder="Search">
-	    </form>
-	    <div class="header_buttons">
-		<a href="browse_by.php" class="browse">Browse By</a>
-		<a href="new_line.php">+ Line</a>
-		<?php //logged in user name
-		    
-		    $logged_in_username='brannondorsey'; //$_GET('logged_in_username');
-		    $user_first = "Brannon"; //SHOW first_name FROM user WHERE username = $logged_in_username;
-		    $user_last = "Dorsey"; //SHOW last_name FROM user WHERE username = $logged_in_username;
-		    $logged_in_name = ucfirst($user_first).' '.ucfirst($user_last);
-		    
-		    //<a> string
-		    echo "<a href='user.php?logged_in_username=$logged_in_username'
-		    class='user_name' style='color: #303030; border: none; padding-left: 1em;'
-		    >$logged_in_name</a>";
-		?>
-		<!-- <img src="assets/carabiner.png" class="carabiner"> -->
-	    </div> <!-- header_buttons -->
+        	<div class="left">
+			    <a href="home.php" class="homepage_link">
+					<img src="assets/login_logo.png" class="header_logo" width="60em">
+					<h1>Linelocker</h1> 
+			    </a>
+        	</div>
+        	<div class="right">
+			    <form>
+					<input type="text" name="search" placeholder="Search">
+			    </form>
+			    <div class="header_buttons">
+					<a href="browse_by.php" class="browse">Browse By</a>
+					<a href="new_line.php">+ Line</a>
+			<?php //logged in user name
+			    
+			    $logged_in_username='brannondorsey'; //$_GET('logged_in_username');
+			    $user_first = "Brannon"; //SHOW first_name FROM user WHERE username = $logged_in_username;
+			    $user_last = "Dorsey"; //SHOW last_name FROM user WHERE username = $logged_in_username;
+			    $logged_in_name = ucfirst($user_first).' '.ucfirst($user_last);
+			    
+			    //<a> string
+			    echo "<a href='user.php?logged_in_username=$logged_in_username'
+			    class='user_name' style='color: #303030; border: none; padding-left: 1em;'
+			    >$logged_in_name</a>";
+			?>
+			<!-- <img src="assets/carabiner.png" class="carabiner"> -->
+		    	</div> <!-- header_buttons -->
+        	</div>
 	</header>
 	
 	<div class="line_container">
