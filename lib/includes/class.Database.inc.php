14c7f3c1cd3d7e0327c8d915544ab0d59d1b04d4
Kevin Zweerink <kevin@kevinzweerink.com>
2013-01-15 17:52:53 -0500

    Adding result output to db class

--- lib/includes/class.Database.inc.php
+++ lib/includes/class.Database.inc.php
@@ -6,14 +6,15 @@ class Database {
 	protected $password = "root";
 	protected $db       = "linelocker";
 	protected $host     = "localhost";
+	public $result;
 	protected $connection;
 	
 	public function execute_sql($query) {
 		
 		$this->connection = new mysqli($this->host, $this->user, $this->password, $this->db);
 		$this->connection->query($query);
+		$this->result = $this->connection->fetch_assoc();
 		$this->connection->close();
-		
 	}
 
 }
--- lib/includes/test.php
+++ lib/includes/test.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'class.User.inc.php';
+/*require_once 'class.User.inc.php';
 
 $first_name = "Kevin";
 $last_name = "Zweerink";
@@ -15,7 +15,15 @@ $country = "United States";
 
 
 $user = new User($first_name, $last_name, $username, $password, $email, $experience, $equipment, $city, $state, $country);
-$user->create_user();
+$user->create_user();*/
+
+require_once 'class.Database.inc.php';
+
+$statement = "SELECT first_name FROM users WHERE last_name='Zweerink'";
+
+$connection = new Database();
+$connection->execute_sql($statement);
+var_dump($connection->result);
 
 
 
