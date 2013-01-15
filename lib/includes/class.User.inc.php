e76b874d29e96f37a0aa27f83058302e3ea16188
Kevin Zweerink <kevin@kevinzweerink.com>
2013-01-15 15:12:13 -0500

    Working on the user class

--- lib/includes/class.User.inc.php
+++ lib/includes/class.User.inc.php
@@ -7,6 +7,7 @@ class User {
     private $last_name;
     private $username;
     private $password;
+    private $confirm;
     private $id;
     private $email;
     private $experience;
@@ -15,16 +16,20 @@ class User {
     private $state;
     private $country;
     
+    private $validation_missing = array();
+    private $validation_messages = array();
+    
     private $display_name;
     
     private $db;
     private $sql;
     
-    public function __construct ($first_name, $last_name, $username, $password, $email, $experience, $equipment=NULL, $city, $state, $country) {
+    public function __construct ($first_name, $last_name, $username, $password, $confirm, $email, $experience, $equipment=NULL, $city, $state, $country) {
         $this->first_name   = $first_name;
         $this->last_name    = $last_name;
-        $this->username     = $username;
+        $this->username     = strtolower($first_name).strtolower($last_name);
         $this->password     = $password;
+        $this->confirm      = $confirm
         $this->email        = $email;
         $this->experience   = $experience;
         $this->equipment    = $equipment;
@@ -45,11 +50,33 @@ class User {
         
     }
     
-     public function edit_user() {
+    public function edit_user() {
         
     }
     
-     public function delete_user() {
+    public function prep_user_info() {
+	    if (!$this->first_name) {
+	    	array_push($this->validation_missing, 'first name');
+	    } else if (!$this->last_name) {
+	    	array_push($this->validation_missing, 'last name');
+	    } else if (!$this->password) {
+	    	array_push($this->validation_missing, 'password');
+	    } else if ($this->password != $this->confirm) {
+		    array_push($this->validation_errors, "Passwords don't match.");
+	    } else if (!this->email) {
+		    array_push ($this->validation_missing, 'email');
+	    } else if (!preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/',$this->email) {
+		    array_push ($this->validation_errors, 'Please use a valid e-mail.');
+	    } else if ()
+	    
+	    
+	    
+	    
+    }
+    
+    
+    
+    public function delete_user() {
         
     }
 
