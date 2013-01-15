151fb5133fee1365db8bdab24c84757b5d05714d
Brannon Dorsey <brannon@brannondorsey.com>
2013-01-15 14:41:54 -0600

    extending class Line to Spot did not work. Reverting Changes.

--- lib/includes/class.Spot.inc.php
+++ lib/includes/class.Spot.inc.php
@@ -1,22 +1,33 @@
 <?php
 
 require_once 'class.Database.inc.php';
-require_once 'class.Line.inc.php';
 
-class Spot extends Line{
+class Spot{
     
+    private $city;
+    private $state;
+    private $location;
     private $rating;
     private $review;
+    private $creator;
+    private $user;
+    
     private $average_rating;
+    private $db;
+    
+    
+    
     
     public function __construct($city, $state, $location, $rating, $review, $creator, $user){
-        parent::$this->city     = $city;
-        parent::$this->state    = $state;
-        parent::$this->location = $location;
-        $this->rating           = $rating;
-        $this->review           = $review;
-        parent::$this->creator 	= $creator;
-        parent::$this->user     = $user;
+        $this->city     = $city;
+        $this->state    = $state;
+        $this->location = $location;
+        $this->rating   = $rating;
+        $this->review   = $review;
+        $this->creator 	= $creator;
+        $this->user     = $user;
+        $this->db       = new Database();
+        
     }
     
     private function average_rating() {
@@ -27,19 +38,20 @@ class Spot extends Line{
         $this->sql = "INSERT INTO spot (city, state_province, location, rating, review, creator, user) VALUES ('$this->city', '$this->state', '$this->location', '$this->rating', '$this->review', '$this->creator', '$this->user')";
         $this->db->execute_sql($this->sql);
     }
+ 
+        public function rate_review_spot($r) {
         
-    public function rate_review_spot($r) {
-        //rate and or review spot
         $this->sql = "INSERT INTO spot $r VALUES '$this->$r'";
         $this->db->execute_sql($this->sql);
         
         $this->average_rating();
     }
-
     
     public function display_spot() {
         
     }
+    
+   
 }
 ?>
 
