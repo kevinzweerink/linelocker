bfa4e62801088fd31dfda2036dfbc5215fd430f0
Kevin Zweerink <kevin@kevinzweerink.com>
2013-01-16 10:49:41 -0500

    adding validation to line class

--- lib/includes/class.Line.inc.php
+++ lib/includes/class.Line.inc.php
@@ -18,11 +18,16 @@ class Line {
 	private $date;
 	private $time;
 	private $message;
-	private $sql;
 	
+	private $line_info_ready=NULL;
+	private $error_messages;
+	private $validation_messages;
+	private $validation_missing;
+	
+	private $sql;
 	private $db;
 	
-	public function __construct($city, $state, $location, $length, $type, $width, $creator, $users, $equipment_accounted, $equipment_needed, $date, $time, $message) {
+	public function __construct($city=NULL, $state=NULL, $location=NULL, $length=NULL, $type=NULL, $width=NULL, $creator=NULL, $users=NULL, $equipment_accounted=NULL, $equipment_needed=NULL, $date=NULL, $time=NULL, $message=NULL) {
 		$this->city                = $city;
 		$this->state               = $state;
 		$this->location            = $location;
@@ -40,11 +45,75 @@ class Line {
 	
 	}
 	
+	public function format_messages() {
+		if (!empty($this->validation_missing)) {
+		    $this->message .= "<p>Please fill out the following fields:";
+		    foreach ($this->validation_missing as $missing) {
+			    $this->message .= " ".$missing;
+		    }
+		    $this->message .= ".</p>";
+	    }
+	    if (!empty ($this->validation_messages)) {
+		    foreach ($this->validation_messages as $messages) {
+			    $this->message .= "<p>";
+			    $this->message .= $messages;
+			    $this->message .= "</p>";
+		    }
+	    }
+	}
+	
+	public function prep_user_info() {
+    
+	    if (!$this->city) {
+	    	array_push($this->validation_missing, 'city');
+	    } 
+	    if (!$this->state) {
+	    	array_push($this->validation_missing, 'city');
+	    } 
+	    if (!$this->location) {
+	    	array_push($this->validation_missing, 'location');
+	    } 
+	    if (!$this->length) {
+		    array_push ($this->validation_missing, 'length');
+	    } 
+	    if (!this->type) {
+		    array_push ($this->validation_missing, 'type');
+	    } 
+	    if (!$this->width) {
+		    array_push ($this->validation_missing, 'width');
+	    } 
+	    if (!$this->equipment_accounted) {
+		    array_push ($this->validation_missing, 'equipment accounted for');
+	    } 
+	    if (!$this->date) {
+		    array_push($this->validation_missing, 'date');
+	    }
+	    if (!$this->time) {
+		    array_push($this->validation_missing, 'time');
+	    }
+	    if (!$this->message) {
+		    array_push($this->validation_missing, 'message');
+	    }
+	    
+	    if (!empty($this->validation_missing) || !empty($this->validation_messages)) {
+		   	$this->format_messages();
+	    } else {
+		   	$this->user_info_ready = TRUE;
+	    }
+	  
+    }
+	
 	public function create_line() {
+	
+		if ($this->line_info_ready) {
 	
 		$this->sql = "INSERT INTO line (city, state_province, location, length, type, width, creator, users, equipment_accounted, equipment_needed, date, time, message) VALUES ('$this->city', '$this->state', '$this->location', '$this->length', '$this->type', '$this->width', '$this->creator', '$this->users', '$this->equipment_accounted', '$this->equipment_needed', '$this->date', '$this->time', '$this->message')";
 		
 		$this->db->execute_sql($this->sql);
+		
+		} else {
+			$this->format_messages();
+		}
 				
 	}
 	
