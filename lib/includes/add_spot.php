d565b1395cc3d2100fb247e67d3eaf7cffd5c77d
Brannon Dorsey <brannon@brannondorsey.com>
2013-01-17 17:08:52 -0600

    c

---
+++ lib/includes/add_spot.php
@@ -0,0 +1,12 @@
+<form class="rating" method="post">
+  <input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="5/5">&#9733</label>
+  <input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="4/5">&#9733</label>
+  <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="3/5">&#9733</label>
+  <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="2/5">&#9733</label>
+  <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="1/5">&#9733</label>
+  
+  <textarea ></textarea>
+  
+  <input type="submit" value="submit">
+</form>
+
